2ed38b43ebbacaa2c638216d133852fb995f31b3
ytodorov <ytodorov@ytodorov.com>
2016-01-21 23:14:52 +0200

    expreimenting ,,

---
+++ src/JQuery-ExamplesSln/Jquery-ExamplesWebsite/Scripts/YordanCreateCodeEditors.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var textAreas = $("textarea.code");
+    for (var i = 0; i < textAreas.length; i++) {
+        debugger;
+        var ec = {};
+        ec.tabSize = 2;
+        ec.lineNumbers = true;
+        ec.theme = "neat";
+        ec.matchBrackets = true;
+        debugger;
+        var editor = CodeMirror.fromTextArea($(textAreas).get(i), {
+            lineNumbers: true,
+            matchBrackets: true,
+            theme: "neat"
+        });
+    }
+});
+//# sourceMappingURL=YordanCreateCodeEditors.js.map
---
+++ src/JQuery-ExamplesSln/Jquery-ExamplesWebsite/Scripts/YordanCreateCodeEditors.ts
@@ -0,0 +1,22 @@
+﻿$(document).ready(() => {
+
+    var textAreas = $("textarea.code");
+
+    for (var i = 0; i < textAreas.length; i++) {
+        debugger;
+        var ec: CodeMirror.EditorConfiguration = <CodeMirror.EditorConfiguration>{};
+        ec.tabSize = 2;
+        ec.lineNumbers = true;
+        ec.theme = "neat";
+        ec.matchBrackets = true;
+        debugger;
+        var editor = CodeMirror.fromTextArea($(textAreas).get(i) as HTMLTextAreaElement, {
+            lineNumbers: true,
+            matchBrackets: true,
+            theme: "neat"
+        });
+    }    
+});
+
+
+   
